7259dda228be6fe70afac71d0176e94cbff03e75
Pramod Jacob Sam <pramodjsam@gmail.com>
2022-12-20 18:10:52 -0500

    dark mode completed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import "./navbar.scss";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Navbar = () => {
+  const { dispatch, darkMode } = useContext(DarkModeContext);
+
   return (
     <div className="navbar">
       <div className="wrapper">
@@ -22,7 +26,17 @@ const Navbar = () => {
             English
           </div>
           <div className="item">
-            <DarkModeOutlinedIcon className="icon" />
+            {darkMode ? (
+              <LightModeOutlinedIcon
+                className="icon"
+                onClick={() => dispatch!({ type: "TOGGLE" })}
+              />
+            ) : (
+              <DarkModeOutlinedIcon
+                className="icon"
+                onClick={() => dispatch!({ type: "TOGGLE" })}
+              />
+            )}
           </div>
           <div className="item">
             <FullscreenExitOutlinedIcon className="icon" />
