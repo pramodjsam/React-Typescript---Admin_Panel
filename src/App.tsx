e62f1b0b4b479e3e85e320a9d8aeb37ed8c66d62
Pramod Jacob Sam <pramodjsam@gmail.com>
2022-12-20 05:24:42 -0500

    react-router-dom setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 
 function App() {
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userid" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productid" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
